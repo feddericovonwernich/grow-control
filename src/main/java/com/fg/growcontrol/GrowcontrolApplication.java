ab35721e2a6ea0212c8ffbb4e5cea9df4fb41327
Guido Morales <guidom06@hotmail.com>
2023-09-13 21:33:29 -0300

    Subiendo creacion de proyecto

---
+++ src/main/java/com/fg/growcontrol/GrowcontrolApplication.java
@@ -0,0 +1,13 @@
+package com.fg.growcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowcontrolApplication.class, args);
+	}
+
+}
